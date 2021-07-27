115074c5a781f09e138ec69bb9b5a6a8f7b1ebc4
Lenovo <FoysalAhmedCSE45@gmail.com>
2021-07-27 17:49:04 +0600

    Initial commit

--- app/src/main/java/com/example/testproject/Design.java
+++ app/src/main/java/com/example/testproject/Design.java
@@ -1,14 +1,39 @@
 package com.example.testproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.testproject.databinding.ActivityDesignBinding;
 
 public class Design extends AppCompatActivity {
-
+//ActivityDesignBinding activityDesignBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_design);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(false);
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setDisplayUseLogoEnabled(false);
+            actionBar.setTitle("WebView");
+        }
+     //  https://www.geeksforgeeks.org/
+        WebView webView=findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.geeksforgeeks.org/");
     }
-}
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+    }
+
+
--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.testproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        initview()
         //actionbar
         val actionbar = supportActionBar
         actionbar?.hide()
+        binding.button2.setOnClickListener {
+            startActivity(
+
+                Intent(this, Design::class.java)
+
+            )
+        }
+
+    }
+    private fun initview() {
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
